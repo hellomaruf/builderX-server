7f505a636435ee8d9a4faa4d63ba13bb77b80cdb
hellomaruf <marufahamed1110@gmail.com>
2025-11-03 19:15:11 -0500

    fix issues

---
+++ index.js
@@ -0,0 +1,99 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const { MongoClient } = require("mongodb");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+console.log(uri);
+
+async function run() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("authentication");
+    const collection = db.collection("users");
+
+    // User Registration
+    app.post("/api/v1/register", async (req, res) => {
+      const { username, email, password } = req.body;
+
+      const existingUser = await collection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: "User already exist!!!",
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await collection.insertOne({
+        username,
+        email,
+        password: hashedPassword,
+        role: "user",
+      });
+
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully!",
+      });
+    });
+
+    // User Login
+    app.post("/api/v1/login", async (req, res) => {
+      const { email, password } = req.body;
+
+      const user = await collection.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      const token = jwt.sign(
+        { email: user.email, role: user.role },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.EXPIRES_IN,
+        }
+      );
+
+      res.json({
+        success: true,
+        message: "User successfully logged in!",
+        accessToken: token,
+      });
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running smoothly",
+    timestamp: new Date(),
+  });
+});
